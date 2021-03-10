d1038d0249b70a861e9b4ea2f1bfbb9a2b8a2d42
Chetan Dhamija <cdhamija_admin@euc.vmwarepso.org>
2021-03-10 18:26:15 -0500

    Add project files.

---
+++ CustomApplication/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace CustomApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(523, 451);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.richTextBox1);
+            this.panel2.Location = new System.Drawing.Point(12, 297);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(495, 126);
+            this.panel2.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(495, 126);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(185, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "OKTA ADMIN TOOL";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.searchBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button6, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button7, 0, 4);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 68);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.91667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.08333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(495, 198);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(241, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CREATE USER";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(250, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(242, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "GET USER";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(250, 128);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(242, 33);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "DELETE USER";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button5.Location = new System.Drawing.Point(3, 83);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(241, 39);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "ACTIVATE USER";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Location = new System.Drawing.Point(250, 83);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(242, 39);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "DEACTIVATE USER";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // searchBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.searchBox, 2);
+            this.searchBox.Location = new System.Drawing.Point(3, 3);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(489, 20);
+            this.searchBox.TabIndex = 7;
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button6.Location = new System.Drawing.Point(3, 128);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(241, 33);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "ADD USER TO ADMIN GROUP";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button7.Location = new System.Drawing.Point(3, 167);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(241, 28);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "OKTA PORTAL";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 450);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "OKTA USERS MANAGEMENT";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ CustomApplication/Form1.cs
@@ -0,0 +1,281 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace CustomApplication
+{
+    public partial class Form1 : Form
+    {
+        //PUBLIC VARIABLES
+        public string UserId { get; set; }
+
+        public string SearchText { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string GetUser()
+        {
+            try
+            {
+                string query = searchBox.Text;
+                var client = new RestClient("https://dev-10864403-admin.okta.com/api/v1/users?q=" + query + "&limit=1");
+                client.Timeout = -1;
+                var request = new RestRequest(Method.GET);
+                request.AddHeader("Accept", "application/json");
+                request.AddHeader("Content-Type", "application/json");
+                request.AddHeader("Authorization", "SSWS 00uofXk0ZmwBdmaNLohHAtdQx9p5zAhp6sVFEX28We");
+                request.AddHeader("Cookie", "JSESSIONID=47ACCD23AF835C13A4EF68EA23159683");
+                IRestResponse response = client.Execute(request);
+                string responset1 = response.Content.TrimStart('[');
+                string responset2 = responset1.TrimEnd(']');
+                var oktaUser = JsonConvert.DeserializeObject<OktaUser.Root>(responset2);
+                if (oktaUser == null)
+                { 
+                    richTextBox1.Text = "User not found.";
+                    return null;
+                }
+                else
+                {
+                    UserId = oktaUser.id;
+                    return UserId;
+                }
+            }
+            catch
+            {  
+                richTextBox1.Text = "Please enter a username in search box.";
+                return null;
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //CREATE USER
+           
+            var client = new RestClient("https://dev-10864403-admin.okta.com/api/v1/users?activate=false");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authorization", "SSWS 00uofXk0ZmwBdmaNLohHAtdQx9p5zAhp6sVFEX28We");
+            request.AddHeader("Cookie", "JSESSIONID=958E54031AF1D4E163B5786136211898");
+           
+            request.AddParameter("application/json", "{\n  \"profile\": {\n    \"firstName\": \"Test\",\n    \"lastName\": \"User\",\n    \"email\": \"tuser2@oktabank.com\",\n    \"login\": \"tuser2@oktabank.com\"\n  },\n  \"credentials\": {\n    \"password\" : { \"value\": \"VMware@123\" }\n  }\n}", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+
+            richTextBox1.Text = "USER CREATED: tuser2@oktabank.com";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //GET USER 
+            if (GetUser() != null)
+                richTextBox1.Text = "Found user with UserId = " + GetUser();
+            else
+                richTextBox1.Text = "User not found.";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // DEACTIVATE USER
+            try
+            {
+                UserId = GetUser();
+                var client2 = new RestClient("https://dev-10864403-admin.okta.com/api/v1/users/" + UserId + "/lifecycle/deactivate");
+                client2.Timeout = -1;
+                var request2 = new RestRequest(Method.POST);
+                request2.AddHeader("Content-Type", "application/json");
+                request2.AddHeader("Accept", "application/json");
+                request2.AddHeader("Authorization", "SSWS 00uofXk0ZmwBdmaNLohHAtdQx9p5zAhp6sVFEX28We");
+                request2.AddHeader("Cookie", "JSESSIONID=885DDD6FBC8568DCD0B1E69FBD66C027");
+                request2.AddParameter("application/json", "", ParameterType.RequestBody);
+                IRestResponse response2 = client2.Execute(request2);
+                richTextBox1.Text = "User deactivated";
+            }
+            catch
+            {
+                richTextBox1.Text = "Please enter a username in search box.";
+            }          
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //DELETE USER
+
+            try
+            {
+                string query = searchBox.Text;
+                var client = new RestClient("https://dev-10864403-admin.okta.com/api/v1/users?q=" + query + "&limit=1");
+                client.Timeout = -1;
+                var request = new RestRequest(Method.GET);
+                request.AddHeader("Accept", "application/json");
+                request.AddHeader("Content-Type", "application/json");
+                request.AddHeader("Authorization", "SSWS 00uofXk0ZmwBdmaNLohHAtdQx9p5zAhp6sVFEX28We");
+                request.AddHeader("Cookie", "JSESSIONID=47ACCD23AF835C13A4EF68EA23159683");
+                IRestResponse response = client.Execute(request);
+                string responset1 = response.Content.TrimStart('[');
+                string responset2 = responset1.TrimEnd(']');
+                var oktaUser = JsonConvert.DeserializeObject<OktaUser.Root>(responset2);
+                UserId = oktaUser.id;
+                richTextBox1.Text = response.Content;
+            }
+            catch
+            {
+                richTextBox1.Text = "Please enter a username in search box.";
+            }
+
+            try
+            {
+                var client = new RestClient("https://dev-10864403-admin.okta.com/api/v1/users/"+UserId);
+                client.Timeout = -1;
+                var request = new RestRequest(Method.DELETE);
+                request.AddHeader("Accept", "application/json");
+                request.AddHeader("Content-Type", "application/json");
+                request.AddHeader("Authorization", "SSWS 00uofXk0ZmwBdmaNLohHAtdQx9p5zAhp6sVFEX28We");
+                request.AddHeader("Cookie", "JSESSIONID=2AA8E87C3F65D0C69D21AD1B5ADCFC4D");
+                IRestResponse response = client.Execute(request);
+                richTextBox1.Text = response.Content;
+            }
+            catch { richTextBox1.Text = "Please enter a username in search box."; }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // ACTIVATE USER
+            try
+            {
+                var client = new RestClient("https://dev-10864403-admin.okta.com/api/v1/users/" + UserId + "/lifecycle/activate?sendEmail=false");
+                client.Timeout = -1;
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Content-Type", "application/json");
+                request.AddHeader("Accept", "application/json");
+                request.AddHeader("Authorization", "SSWS 00uofXk0ZmwBdmaNLohHAtdQx9p5zAhp6sVFEX28We");
+                request.AddHeader("Cookie", "JSESSIONID=9696BF8793A369BD661FBA8F0A8751AD");
+                request.AddParameter("application/json", "", ParameterType.RequestBody);
+                IRestResponse response = client.Execute(request);
+                richTextBox1.Text = response.Content;
+            }
+            catch
+            {
+                richTextBox1.Text = "Please enter a username in search box.";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //ADD USER TO ADMIN GROUP
+            try
+            {
+                UserId = GetUser();
+                var client = new RestClient("https://dev-10864403-admin.okta.com/api/v1/groups/00gbdx3h5BXpaArdL5d6/users/"+UserId);
+                client.Timeout = -1;
+                var request = new RestRequest(Method.PUT);
+                request.AddHeader("Accept", "application/json");
+                request.AddHeader("Content-Type", "application/json");
+                request.AddHeader("Authorization", "SSWS 00uofXk0ZmwBdmaNLohHAtdQx9p5zAhp6sVFEX28We");
+                request.AddHeader("Cookie", "JSESSIONID=9696BF8793A369BD661FBA8F0A8751AD");
+                request.AddParameter("application/json", "", ParameterType.RequestBody);
+                IRestResponse response = client.Execute(request);
+                richTextBox1.Text = "User added to admin group.";
+            }
+            catch
+            {
+                richTextBox1.Text = "Cannot add user to Admin group";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+             System.Diagnostics.Process.Start("https://ubiquitous-summer-team.glitch.me");
+        }
+    }
+
+    internal class OktaUser
+    {
+        public class Type
+        {
+            public string id { get; set; }
+        }
+
+        public class Profile
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public object mobilePhone { get; set; }
+            public object secondEmail { get; set; }
+            public string login { get; set; }
+            public string email { get; set; }
+        }
+
+        public class Password
+        {
+        }
+
+        public class Email
+        {
+            public string value { get; set; }
+            public string status { get; set; }
+            public string type { get; set; }
+        }
+
+        public class Provider
+        {
+            public string type { get; set; }
+            public string name { get; set; }
+        }
+
+        public class Credentials
+        {
+            public Password password { get; set; }
+            public List<Email> emails { get; set; }
+            public Provider provider { get; set; }
+        }
+
+        public class Self
+        {
+            public string href { get; set; }
+        }
+
+        public class Links
+        {
+            public Self self { get; set; }
+        }
+
+        public class Root
+        {
+            public string id { get; set; }
+            public string status { get; set; }
+            public DateTime created { get; set; }
+            public object activated { get; set; }
+            public object statusChanged { get; set; }
+            public object lastLogin { get; set; }
+            public DateTime lastUpdated { get; set; }
+            public DateTime? passwordChanged { get; set; }
+            public Type type { get; set; }
+            public Profile profile { get; set; }
+            public Credentials credentials { get; set; }
+            public Links _links { get; set; }
+        }
+    }
+}
